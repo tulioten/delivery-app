652b2c1e1ea25a81c53df4f3bcdaaab4953bf682
tulioten <tuliotenorio0@gmail.com>
2024-06-24 16:25:33 -0300

    new category/edit category - span fixed

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -8,7 +8,7 @@ export async function POST(req) {
 
 export async function PUT(req) {
   const { _id, name } = await req.json()
-  await Category.updateOne({ _id, name })
+  await Category.updateOne({ _id }, { name })
   return Response.json(true)
 }
 
--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -62,7 +62,7 @@ export default function CategoriesPage() {
         })
         setCategoryName('')
         fetchCategories()
-        setCategoryName('')
+        setEditedCategory(null)
         if (response.ok) {
           return resolve()
         } else {
